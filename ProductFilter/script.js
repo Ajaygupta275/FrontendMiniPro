8e750a8bb1d8cf2e5f9b0bd4a54c46009880e5d1
ajaygupta002 <ajaygupta@sidpik.com>
2025-07-23 16:21:38 +0530

    Filter-Product

---
+++ ProductFilter/script.js
@@ -0,0 +1,404 @@
+let products={
+    data:
+    //     {
+    //         productName:"Regular White T-Shirt",
+    //         category:"Topwear",
+    //         price:'30',
+    //         image:"white-tshirt.jpg"
+
+    //     },
+    //     {
+    //         productName:"Beign Short skirt",
+    //         category:"Bootwear",
+    //         price:'49',
+    //         image:"sort-skirt.jpg"
+
+    //     },
+    //     {
+    //         productName:"Regular White T-Shirt",
+    //         category:"Topwear",
+    //         price:'30',
+    //         image:"white-tshirt.jpg"
+
+    //     },
+    //     {
+    //         productName:"Regular White T-Shirt",
+    //         category:"Topwear",
+    //         price:'30',
+    //         image:"white-tshirt.jpg"
+
+    //     },
+    //     {
+    //         productName:"Regular White T-Shirt",
+    //         category:"Topwear",
+    //         price:'30',
+    //         image:"white-tshirt.jpg"
+
+
+    //     },
+    //     {
+    //         productName:"Regular White T-Shirt",
+    //         category:"Topwear",
+    //         price:'30',
+    //         image:"white-tshirt.jpg"
+
+    //     }
+    // ]
+    [
+  {
+    "productName": "Floral Summer Dress",
+    "category": "Topwear",
+    "price": "129",
+    "image": "https://natalet.com/images/floral-summer-dress/floral-summer-dress-34-5.jpg"
+  },
+  {
+    "productName": "Bomber Jacket",
+    "category": "Bottomwear",
+    "price": "135",
+    "image": "https://i.pinimg.com/736x/f5/60/8a/f5608aa06219a728fc867474da79148b.jpg"
+  },
+  {
+    "productName": "Embroidered Tunic",
+    "category": "Footwear",
+    "price": "73",
+    "image": "https://ayurvastram.com/cdn/shop/products/Embroidered_Cotton_tunic_red.jpg?v=1521059564&width=352"
+  },
+  {
+    "productName": "Track Pants",
+    "category": "Ethnicwear",
+    "price": "130",
+    "image": "https://assets.myntassets.com/h_200,w_200,c_fill,g_auto/h_1440,q_100,w_1080/v1/assets/images/19898120/2022/9/12/5a5c0afe-9ba7-468b-85c8-530fb3f57e371662981968972Q-RiousSolidWomensTrackPants1.jpg"
+  },
+  {
+    "productName": "Tank Top",
+    "category": "Accessories",
+    "price": "48",
+    "image": "https://th.bing.com/th/id/R.1145b9da0d488e1fdde8a65012629a0c?rik=BaCmk%2bWmDXfPlQ&riu=http%3a%2f%2fimage26.stylesimo.com%2fo_img%2f2018%2f05%2f02%2f254175-10541198%2ffashion-solid-sleeveless-whorl-shape-high-neck-women-knitr-tank-top.jpg&ehk=tF%2b6lyEAMVsgeIvzDJyJMGGlGGDfchvLdFf1QNK5mSI%3d&risl=&pid=ImgRaw&r=0"
+  },
+  {
+    "productName": "Jeggings",
+    "category": "Footwear",
+    "price": "90",
+    "image": "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/679478/64/mod01/fnd/IND/fmt/png/Women's-High-Waist-7/8-Leggings"
+  },
+  {
+    "productName": "Thermal Innerwear",
+    "category": "Activewear",
+    "price": "55",
+    "image": "https://5.imimg.com/data5/SELLER/Default/2021/10/UC/DI/WB/12823046/ooshin-uk-gold-men-thermal-set-500x500.jpg"
+  },
+  {
+    "productName": "Long Sleeve Turtleneck",
+    "category": "Topwear",
+    "price": "41",
+    "image": "https://www.jiomart.com/images/product/500x630/441153292_navy/long-sleeve-turtleneck-sweater-model-441153292_navy-0-202301090414.jpg"
+  },
+  {
+    "productName": "Printed Kurti",
+    "category": "Ethnicwear",
+    "price": "132",
+    "image": "https://th.bing.com/th/id/R.248ccbaaab46e5c4a63fc8629f4dce43?rik=FYN1ZV%2fYjTkJHA&riu=http%3a%2f%2f5.imimg.com%2fdata5%2fANDROID%2fDefault%2f2023%2f4%2f300822235%2fZE%2fUQ%2fXV%2f14267355%2fproduct-jpeg-1000x1000.jpg&ehk=H6QhVU2%2fZHyRqxPXlPHX3e5Y78c8UTLQChV6on8jDcI%3d&risl=&pid=ImgRaw&r=0"
+  },
+  {
+    "productName": "Knitted Wool Sweater",
+    "category": "Topwear",
+    "price": "97",
+    "image": "https://cdn11.bigcommerce.com/s-scgdirr/images/stencil/480x633/products/23673/68200/39__22526.1617035245.jpg?c=2"
+  },
+  {
+    "productName": "Slim Fit Jeans",
+    "category": "Bottomwear",
+    "price": "61",
+    "image": "https://escollection.es/25004-superlarge_default/slim-fit-jeans.jpg"
+  },
+  {
+    "productName": "Classic Blue Denim Jacket",
+    "category": "Outerwear",
+    "price": "58",
+    "image": "https://tse3.mm.bing.net/th/id/OIP.sbqLkvj2caV3W3YZtx-nUwHaJQ?rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    "productName": "Peplum Top",
+    "category": "Topwear",
+    "price": "124",
+    "image": "peplum-top.jpg"
+  },
+  {
+    "productName": "Winter Gloves",
+    "category": "Accessories",
+    "price": "60",
+    "image": "winter-gloves.jpg"
+  },
+  {
+    "productName": "Fleece Jacket",
+    "category": "Outerwear",
+    "price": "122",
+    "image": "fleece-jacket.jpg"
+  },
+  {
+    "productName": "Overcoat",
+    "category": "Outerwear",
+    "price": "143",
+    "image": "overcoat.jpg"
+  },
+  {
+    "productName": "Rain Jacket",
+    "category": "Outerwear",
+    "price": "65",
+    "image": "rain-jacket.jpg"
+  },
+  {
+    "productName": "Button-down Shirt",
+    "category": "Topwear",
+    "price": "116",
+    "image": "button-down-shirt.jpg"
+  },
+  {
+    "productName": "Hooded Sweatshirt",
+    "category": "Outerwear",
+    "price": "111",
+    "image": "hooded-sweatshirt.jpg"
+  },
+  {
+    "productName": "High Heels Sandals",
+    "category": "Footwear",
+    "price": "77",
+    "image": "high-heels-sandals.jpg"
+  },
+  {
+    "productName": "Palazzo Pants",
+    "category": "Bottomwear",
+    "price": "150",
+    "image": "palazzo-pants.jpg"
+  },
+  {
+    "productName": "Linen Shirt",
+    "category": "Topwear",
+    "price": "94",
+    "image": "linen-shirt.jpg"
+  },
+  {
+    "productName": "Sports Running Shoes",
+    "category": "Footwear",
+    "price": "142",
+    "image": "sports-running-shoes.jpg"
+  },
+  {
+    "productName": "Mesh Sneakers",
+    "category": "Footwear",
+    "price": "93",
+    "image": "mesh-sneakers.jpg"
+  },
+  {
+    "productName": "Graphic Print Tee",
+    "category": "Topwear",
+    "price": "21",
+    "image": "graphic-print-tee.jpg"
+  },
+  {
+    "productName": "Corduroy Trousers",
+    "category": "Bottomwear",
+    "price": "53",
+    "image": "corduroy-trousers.jpg"
+  },
+  {
+    "productName": "Classic Leather Wallet",
+    "category": "Accessories",
+    "price": "86",
+    "image": "classic-leather-wallet.jpg"
+  },
+  {
+    "productName": "Chinos Pants",
+    "category": "Bottomwear",
+    "price": "118",
+    "image": "chinos-pants.jpg"
+  },
+  {
+    "productName": "Casual Loafers",
+    "category": "Footwear",
+    "price": "92",
+    "image": "casual-loafers.jpg"
+  },
+  {
+    "productName": "Striped Maxi Dress",
+    "category": "Dresses",
+    "price": "139",
+    "image": "striped-maxi-dress.jpg"
+  },
+  {
+    "productName": "Silk Saree",
+    "category": "Ethnicwear",
+    "price": "145",
+    "image": "silk-saree.jpg"
+  },
+  {
+    "productName": "Jumpsuit",
+    "category": "Dresses",
+    "price": "105",
+    "image": "jumpsuit.jpg"
+  },
+  {
+    "productName": "Ankle Boots",
+    "category": "Footwear",
+    "price": "108",
+    "image": "ankle-boots.jpg"
+  },
+  {
+    "productName": "Dotted Dungarees",
+    "category": "Bottomwear",
+    "price": "134",
+    "image": "dotted-dungarees.jpg"
+  },
+  {
+    "productName": "Wool Beanie",
+    "category": "Accessories",
+    "price": "27",
+    "image": "wool-beanie.jpg"
+  },
+  {
+    "productName": "Casual Cargo Pants",
+    "category": "Bottomwear",
+    "price": "97",
+    "image": "casual-cargo-pants.jpg"
+  },
+  {
+    "productName": "Formal Blazer",
+    "category": "Outerwear",
+    "price": "147",
+    "image": "formal-blazer.jpg"
+  },
+  {
+    "productName": "Leather Handbag",
+    "category": "Accessories",
+    "price": "144",
+    "image": "leather-handbag.jpg"
+  },
+  {
+    "productName": "Crop Top",
+    "category": "Topwear",
+    "price": "87",
+    "image": "crop-top.jpg"
+  },
+  {
+    "productName": "Athletic Shorts",
+    "category": "Activewear",
+    "price": "62",
+    "image": "athletic-shorts.jpg"
+  },
+  {
+    "productName": "Cotton Polo T-Shirt",
+    "category": "Topwear",
+    "price": "88",
+    "image": "cotton-polo-t-shirt.jpg"
+  },
+  {
+    "productName": "Windbreaker Jacket",
+    "category": "Outerwear",
+    "price": "98",
+    "image": "windbreaker-jacket.jpg"
+  },
+  {
+    "productName": "Puffer Jacket",
+    "category": "Outerwear",
+    "price": "137",
+    "image": "puffer-jacket.jpg"
+  },
+  {
+    "productName": "Leather Wallet",
+    "category": "Accessories",
+    "price": "95",
+    "image": "leather-wallet.jpg"
+  },
+  {
+    "productName": "Ripped Skinny Jeans",
+    "category": "Bottomwear",
+    "price": "119",
+    "image": "ripped-skinny-jeans.jpg"
+  },
+  {
+    "productName": "Canvas Sneakers",
+    "category": "Footwear",
+    "price": "123",
+    "image": "canvas-sneakers.jpg"
+  },
+  {
+    "productName": "Sports Bra",
+    "category": "Activewear",
+    "price": "64",
+    "image": "sports-bra.jpg"
+  },
+  {
+    "productName": "Trench Coat",
+    "category": "Outerwear",
+    "price": "101",
+    "image": "trench-coat.jpg"
+  }
+]
+
+}
+
+for(let i of products.data){
+    let card=document.createElement("div");
+    card.classList.add("card",i.category,"hide");
+    let imgContainer=document.createElement("div");
+    imgContainer.classList.add("image-container");
+    let image=document.createElement("img");
+    image.setAttribute("src",i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    // container
+    let container=document.createElement("div");
+    container.classList.add("container");
+
+    let name=document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText=i.productName.toUpperCase();
+    container.appendChild(name);
+    
+    // price
+     let price=document.createElement("h6");
+     price.innerText="$"+ i.price;
+     container.appendChild(price);
+
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+     
+
+
+
+  }
+
+  function filterProduct(value){
+    let buttons=document.querySelectorAll(".button-value");
+    buttons.forEach((button)=>{
+      if(value.toUpperCase()==button.innerText.toUpperCase()){
+        button.classList.add("active");
+      }else{
+        button.classList.remove("active");
+      }
+    });
+
+    // Select all cards
+    let element=document.querySelectorAll(".card");
+    element.forEach((element)=>{
+      if(value=="all"){
+        if(value=="all"){
+          element.classList.remove("hide");
+        }else{
+          if(element.classList.contains(value)){
+            element.classList.remove("hide");
+          }else{
+            element.classList.add("hide");
+          }
+        }
+      }
+    })
+
+  }
+
+  // Intially all product list show
+  
+  window.onload=()=>{
+    filterProduct("all");
+  }
